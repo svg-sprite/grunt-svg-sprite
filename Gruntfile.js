59720c4c003c7796b19efe6572f480a8dd2e073c
XhmikosR <xhmikosr@gmail.com>
2021-10-02 16:40:33 +0300

    Switch to xo

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,13 +12,6 @@ module.exports = function(grunt) {
 
     // Project configuration.
     grunt.initConfig({
-        jshint: {
-            all: ['Gruntfile.js', 'tasks/*.js', '<%= nodeunit.tests %>'],
-            options: {
-                jshintrc: '.jshintrc'
-            }
-        },
-
         // Before generating any new files, remove any previously-created files.
         clean: {
             tests: ['tmp']
@@ -99,13 +92,12 @@ module.exports = function(grunt) {
     grunt.loadTasks('tasks');
 
     // These plugins provide necessary tasks.
-    grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
     // Whenever the "test" task is run, first clean the "tmp" dir, then run this
     // plugin's task(s), then test the result.
-    grunt.registerTask('test', ['jshint', 'clean', 'svg_sprite', 'nodeunit']);
+    grunt.registerTask('test', ['clean', 'svg_sprite', 'nodeunit']);
 
     // By default, lint and run all tests.
     grunt.registerTask('default', ['test']);
