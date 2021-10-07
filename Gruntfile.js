d4fa38fe80c2f17eb5eb9467c2566d2fc8ba838c
XhmikosR <xhmikosr@gmail.com>
2021-10-07 09:06:10 +0300

    tests: switch to mocha

--- Gruntfile.js
+++ Gruntfile.js
@@ -79,11 +79,6 @@ module.exports = function(grunt) {
                     }
                 }
             }
-        },
-
-        // Unit tests.
-        nodeunit: {
-            tests: ['test/*_test.js']
         }
     });
 
@@ -92,12 +87,11 @@ module.exports = function(grunt) {
 
     // These plugins provide necessary tasks.
     grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-    // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-    // plugin's task(s), then test the result.
-    grunt.registerTask('test', ['clean', 'svg_sprite', 'nodeunit']);
+    // Whenever the "test" task is run, first clean the "tmp" dir,
+    // then run this plugin's tasks.
+    grunt.registerTask('test', ['clean', 'svg_sprite']);
 
     // By default, lint and run all tests.
-    grunt.registerTask('default', ['test']);
+    grunt.registerTask('default', 'test');
 };
--- test/svg_sprite_test.js
+++ test/svg_sprite_test.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const assert = require('assert').strict;
 const fs = require('fs');
 const path = require('path');
 const svg2png = require('svg2png');
@@ -12,17 +13,17 @@ const looksSame = require('looks-same');
  * @param {String} png                PNG file path
  * @param {String} expected           Expected PNG file path
  * @param {String} diff               Diff file path
- * @param {Object} test               Nodeunit Test
+ * @param {Function} done             Callback
  * @param {String} msg                Message
  */
 // eslint-disable-next-line max-params
-function compareSvg2Png(svg, png, expected, diff, test, msg) {
+function compareSvg2Png(svg, png, expected, diff, done, msg) {
     fs.mkdirSync(path.dirname(png), { recursive: true });
 
     const ecb = function(err) {
         console.log(err);
-        test.ifError(err);
-        test.done();
+        assert.ifError(err);
+        done();
     };
 
     fs.promises.readFile(svg)
@@ -31,9 +32,9 @@ function compareSvg2Png(svg, png, expected, diff, test, msg) {
             fs.promises.writeFile(png, buffer)
                 .then(() => {
                     looksSame(png, expected, (err, result) => {
-                        test.ifError(err);
-                        test.ok(result.equal, `${msg} ${JSON.stringify(result.diffClusters)} ${png}`);
-                        test.done();
+                        assert.ifError(err);
+                        assert.ok(result.equal, `${msg} ${JSON.stringify(result.diffClusters)} ${png}`);
+                        done();
                     });
                     looksSame.createDiff({
                         reference: expected,
@@ -47,53 +48,48 @@ function compareSvg2Png(svg, png, expected, diff, test, msg) {
         .catch(ecb);
 }
 
-exports.svg_sprite = {
-    setUp(done) {
-        // setup here if necessary
-        done();
-    },
-    vertical(test) {
-        test.expect(2);
+describe('grunt-svg-sprite', () => {
+    it('vertical', done => {
         compareSvg2Png(
             path.join(__dirname, '../tmp/svg/vertical.svg'),
             path.join(__dirname, '../tmp/png/vertical.png'),
             path.join(__dirname, 'expected/vertical.png'),
             path.join(__dirname, '../tmp/png/vertical.diff.png'),
-            test,
+            done,
             'The vertical sprite doesn\'t match the expected one!'
         );
-    },
-    horizontal(test) {
-        test.expect(2);
+    });
+
+    it('horizontal', done => {
         compareSvg2Png(
             path.join(__dirname, '../tmp/svg/horizontal.svg'),
             path.join(__dirname, '../tmp/png/horizontal.png'),
             path.join(__dirname, 'expected/horizontal.png'),
             path.join(__dirname, '../tmp/png/horizontal.diff.png'),
-            test,
+            done,
             'The horizontal sprite doesn\'t match the expected one!'
         );
-    },
-    diagonal(test) {
-        test.expect(2);
+    });
+
+    it('diagonal', done => {
         compareSvg2Png(
             path.join(__dirname, '../tmp/svg/diagonal.svg'),
             path.join(__dirname, '../tmp/png/diagonal.png'),
             path.join(__dirname, 'expected/diagonal.png'),
             path.join(__dirname, '../tmp/png/diagonal.diff.png'),
-            test,
+            done,
             'The diagonal sprite doesn\'t match the expected one!'
         );
-    },
-    packed(test) {
-        test.expect(2);
+    });
+
+    it('packed', done => {
         compareSvg2Png(
             path.join(__dirname, '../tmp/svg/packed.svg'),
             path.join(__dirname, '../tmp/png/packed.png'),
             path.join(__dirname, 'expected/packed.png'),
             path.join(__dirname, '../tmp/png/packed.diff.png'),
-            test,
+            done,
             'The packed sprite doesn\'t match the expected one!'
         );
-    }
-};
+    });
+});
