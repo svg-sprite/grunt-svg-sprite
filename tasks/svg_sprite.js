e1199d6cbefa8391d63e04729794ce73b6d70a35
XhmikosR <xhmikosr@gmail.com>
2025-02-20 19:31:06 +0200

    Update svg-sprite to v3.0.0-rc3 and drop Node.js < 18 support
    Also, switch to 2-space indentation like upstream

--- tasks/svg_sprite.js
+++ tasks/svg_sprite.js
@@ -10,81 +10,81 @@
 
 'use strict';
 
-const path = require('path');
+const path = require('node:path');
 const SVGSpriter = require('svg-sprite');
 const picocolors = require('picocolors');
 const figures = require('figures');
 const pretty = require('prettysize');
 
 const rtrim = (str, strip) => {
-    while (str.length > 0 && strip.length > 0 && (str.substr(-strip.length) === strip)) {
-        str = str.substr(0, str.length - strip.length);
-    }
+  while (str.length > 0 && strip.length > 0 && (str.substr(-strip.length) === strip)) {
+    str = str.substr(0, str.length - strip.length);
+  }
 
-    return str;
+  return str;
 };
 
 module.exports = function(grunt) {
-    grunt.registerMultiTask('svg_sprite', 'Takes a folder of SVG images and creates an SVG sprite along with suitable CSS/Sass/LESS/Stylus resources out of them', function() {
-        const done = this.async();
-        let spriter = null;
-        let config = null;
-        const errors = [];
+  grunt.registerMultiTask('svg_sprite', 'Takes a folder of SVG images and creates an SVG sprite along with suitable CSS/Sass/LESS/Stylus resources out of them', function() {
+    const done = this.async();
+    let spriter = null;
+    let config = null;
+    const errors = [];
 
-        // Iterate over all specified file groups.
-        for (const srcFile of this.files) {
-            config = config || this.options({ dest: path.resolve(srcFile.orig.dest) });
-            const cwd = rtrim(path.normalize(srcFile.orig.cwd || ''), path.sep);
-            const cwdAbs = path.resolve(cwd || '.');
-            const expand = Boolean(srcFile.orig.expand);
+    // Iterate over all specified file groups.
+    for (const srcFile of this.files) {
+      config ||= this.options({ dest: path.resolve(srcFile.orig.dest) });
+      const cwd = rtrim(path.normalize(srcFile.orig.cwd || ''), path.sep);
+      const cwdAbs = path.resolve(cwd || '.');
+      const expand = Boolean(srcFile.orig.expand);
 
-            if (spriter === null) {
-                spriter = new SVGSpriter(config);
-                spriter.config.log.on('logging', (transport, level, msg) => {
-                    if (level === 'error') {
-                        errors.push(msg);
-                    }
-                });
-            }
+      if (spriter === null) {
+        spriter = new SVGSpriter(config);
+        spriter.config.log.on('logging', (transport, level, msg) => {
+          if (level === 'error') {
+            errors.push(msg);
+          }
+        });
+      }
 
-            const files = srcFile.src.map(file => {
-                file = path.normalize(file);
-                // The next line is probably too verbose and could be replaced with `path.basename`
-                return path.resolve(cwdAbs, expand && cwd.length > 0 && file.startsWith(cwd + path.sep) ? file.substr(cwd.length + path.sep.length) : file);
-            }).filter(file => {
-                if (!grunt.file.exists(file)) {
-                    grunt.log.warn(`Source file "${file}" not found.`);
-                    return false;
-                }
-
-                return true;
-            });
-
-            for (const file of files) {
-                spriter.add(file, path.relative(cwdAbs, file), grunt.file.read(file));
-            }
+      const files = srcFile.src.map(file => {
+        file = path.normalize(file);
+        // The next line is probably too verbose and could be replaced with `path.basename`
+        return path.resolve(cwdAbs, expand && cwd.length > 0 && file.startsWith(cwd + path.sep) ? file.substr(cwd.length + path.sep.length) : file);
+      }).filter(file => {
+        if (!grunt.file.exists(file)) {
+          grunt.log.warn(`Source file "${file}" not found.`);
+          return false;
         }
 
-        spriter.compile((error, result) => {
-            if (error) {
-                grunt.fail.warn(`\nSVG sprite compilation failed with message: ${error}`, 3);
-            } else {
-                console.log('');
-                for (const mode of Object.values(result)) {
-                    for (const file of Object.values(mode)) {
-                        if (grunt.file.write(file.path, file.contents)) {
-                            grunt.log.writeln(`${picocolors.green(figures.tick)} ${file.relative} ${picocolors.gray(`(${pretty(file.contents.length)})`)}`);
-                        }
-                    }
-                }
+        return true;
+      });
 
-                if (errors.length > 0) {
-                    const errSeparator = `\n${figures.cross} `;
-                    grunt.fail.warn(`\nThe following errors occured:${errSeparator}${errors.join(errSeparator)}\n`);
-                }
+      for (const file of files) {
+        spriter.add(file, path.relative(cwdAbs, file), grunt.file.read(file));
+      }
+    }
+
+    spriter.compile((error, result) => {
+      if (error) {
+        grunt.fail.warn(`\nSVG sprite compilation failed with message: ${error}`, 3);
+      } else {
+        console.log('');
+        for (const mode of Object.values(result)) {
+          for (const file of Object.values(mode)) {
+            if (grunt.file.write(file.path, file.contents)) {
+              grunt.log.writeln(`${picocolors.green(figures.tick)} ${file.relative} ${picocolors.gray(`(${pretty(file.contents.length)})`)}`);
             }
+          }
+        }
 
-            done();
-        });
+        if (errors.length > 0) {
+          const errSeparator = `\n${figures.cross} `;
+          grunt.fail.warn(`\nThe following errors occured:${errSeparator}${errors.join(errSeparator)}\n`);
+        }
+      }
+
+      done();
     });
+  });
 };
--- test/svg_sprite_test.js
+++ test/svg_sprite_test.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const assert = require('assert').strict;
-const fs = require('fs');
-const path = require('path');
+const assert = require('node:assert').strict;
+const fs = require('node:fs');
+const path = require('node:path');
 const svg2png = require('svg2png');
 const looksSame = require('looks-same');
 
@@ -18,78 +18,78 @@ const looksSame = require('looks-same');
  */
 // eslint-disable-next-line max-params
 function compareSvg2Png(svg, png, expected, diff, done, msg) {
-    fs.mkdirSync(path.dirname(png), { recursive: true });
+  fs.mkdirSync(path.dirname(png), { recursive: true });
 
-    const ecb = function(err) {
-        console.log(err);
-        assert.ifError(err);
-        done();
-    };
+  const ecb = function(err) {
+    console.log(err);
+    assert.ifError(err);
+    done();
+  };
 
-    fs.promises.readFile(svg)
-        .then(svg2png)
-        .then(buffer => {
-            fs.promises.writeFile(png, buffer)
-                .then(() => {
-                    looksSame(png, expected, (err, result) => {
-                        assert.ifError(err);
-                        assert.ok(result.equal, `${msg} ${JSON.stringify(result.diffClusters)} ${png}`);
-                        done();
-                    });
-                    looksSame.createDiff({
-                        reference: expected,
-                        current: png,
-                        diff,
-                        highlightColor: '#ff00ff'
-                    }, () => {});
-                })
-                .catch(ecb);
+  fs.promises.readFile(svg)
+    .then(svg2png)
+    .then(buffer => {
+      fs.promises.writeFile(png, buffer)
+        .then(() => {
+          looksSame(png, expected, (err, result) => {
+            assert.ifError(err);
+            assert.ok(result.equal, `${msg} ${JSON.stringify(result.diffClusters)} ${png}`);
+            done();
+          });
+          looksSame.createDiff({
+            reference: expected,
+            current: png,
+            diff,
+            highlightColor: '#ff00ff'
+          }, () => {});
         })
         .catch(ecb);
+    })
+    .catch(ecb);
 }
 
 describe('grunt-svg-sprite', () => {
-    it('vertical', done => {
-        compareSvg2Png(
-            path.join(__dirname, '../tmp/svg/vertical.svg'),
-            path.join(__dirname, '../tmp/png/vertical.png'),
-            path.join(__dirname, 'expected/vertical.png'),
-            path.join(__dirname, '../tmp/png/vertical.diff.png'),
-            done,
-            'The vertical sprite doesn\'t match the expected one!'
-        );
-    });
+  it('vertical', done => {
+    compareSvg2Png(
+      path.join(__dirname, '../tmp/svg/vertical.svg'),
+      path.join(__dirname, '../tmp/png/vertical.png'),
+      path.join(__dirname, 'expected/vertical.png'),
+      path.join(__dirname, '../tmp/png/vertical.diff.png'),
+      done,
+      'The vertical sprite doesn\'t match the expected one!'
+    );
+  });
 
-    it('horizontal', done => {
-        compareSvg2Png(
-            path.join(__dirname, '../tmp/svg/horizontal.svg'),
-            path.join(__dirname, '../tmp/png/horizontal.png'),
-            path.join(__dirname, 'expected/horizontal.png'),
-            path.join(__dirname, '../tmp/png/horizontal.diff.png'),
-            done,
-            'The horizontal sprite doesn\'t match the expected one!'
-        );
-    });
+  it('horizontal', done => {
+    compareSvg2Png(
+      path.join(__dirname, '../tmp/svg/horizontal.svg'),
+      path.join(__dirname, '../tmp/png/horizontal.png'),
+      path.join(__dirname, 'expected/horizontal.png'),
+      path.join(__dirname, '../tmp/png/horizontal.diff.png'),
+      done,
+      'The horizontal sprite doesn\'t match the expected one!'
+    );
+  });
 
-    it('diagonal', done => {
-        compareSvg2Png(
-            path.join(__dirname, '../tmp/svg/diagonal.svg'),
-            path.join(__dirname, '../tmp/png/diagonal.png'),
-            path.join(__dirname, 'expected/diagonal.png'),
-            path.join(__dirname, '../tmp/png/diagonal.diff.png'),
-            done,
-            'The diagonal sprite doesn\'t match the expected one!'
-        );
-    });
+  it('diagonal', done => {
+    compareSvg2Png(
+      path.join(__dirname, '../tmp/svg/diagonal.svg'),
+      path.join(__dirname, '../tmp/png/diagonal.png'),
+      path.join(__dirname, 'expected/diagonal.png'),
+      path.join(__dirname, '../tmp/png/diagonal.diff.png'),
+      done,
+      'The diagonal sprite doesn\'t match the expected one!'
+    );
+  });
 
-    it('packed', done => {
-        compareSvg2Png(
-            path.join(__dirname, '../tmp/svg/packed.svg'),
-            path.join(__dirname, '../tmp/png/packed.png'),
-            path.join(__dirname, 'expected/packed.png'),
-            path.join(__dirname, '../tmp/png/packed.diff.png'),
-            done,
-            'The packed sprite doesn\'t match the expected one!'
-        );
-    });
+  it('packed', done => {
+    compareSvg2Png(
+      path.join(__dirname, '../tmp/svg/packed.svg'),
+      path.join(__dirname, '../tmp/png/packed.png'),
+      path.join(__dirname, 'expected/packed.png'),
+      path.join(__dirname, '../tmp/png/packed.diff.png'),
+      done,
+      'The packed sprite doesn\'t match the expected one!'
+    );
+  });
 });
